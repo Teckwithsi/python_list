5dcef20fa84cec21e8998d089aa8401af9728717
simon <124110714+Teckwithsi@users.noreply.github.com>
2023-02-12 08:29:45 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+mylist = ["fruit", "apple","orange"]
+
+
+
+print(mylist)
+
+
+mylist.append("lemmon")
+
+
+print(mylist)
+
+
+mylist.insert(1,"cheery")
+
+print(mylist)
+
+
+
+item = mylist.remove("orange")
+
+
+print(item)
+
+print(mylist)
+
+#can also cealr all it the clear method
+
+
+#reverse the list with the reverse method as well
+
+
+#sort it as well.
+
+
